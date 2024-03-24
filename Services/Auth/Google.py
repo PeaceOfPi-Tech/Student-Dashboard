f9ada2b6ed1d0003e66ff731c2928ee1b2988603
joshlo39 <83348928+joshlo39@users.noreply.github.com>
2024-03-24 17:24:00 -0400

    Updated student cell on sheet

--- Services/Auth/Google.py
+++ Services/Auth/Google.py
@@ -1,11 +1,13 @@
-from google.oauth2.credentials import Credentials
+from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
 def googleAuth():
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-    creds = Credentials.from_service_account_file('Student_Dashboard_Credentials.json', scopes=SCOPES)
+    creds = Credentials.from_service_account_file('Student_Dashboard_IAM.json', scopes=SCOPES)
     def getSheetServive():
         return build('sheets', 'v4', credentials=creds)
     
     def getDriveService(): 
         return build('drive', 'v3', credentials=creds)
+
+    return getSheetServive, getDriveService
